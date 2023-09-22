ac2323da1fe8f5c70fe52bf2321dabb259a07d55
rnwnsgud <rnwnsgud90@naver.com>
2023-09-22 23:00:19 +0900

    first Commit

---
+++ src/main/java/co/kr/ppingpong/domain/review/Review.java
@@ -0,0 +1,24 @@
+package co.kr.ppingpong.domain.review;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Blob;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Blob text;
+
+}
