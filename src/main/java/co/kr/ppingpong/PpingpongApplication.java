23cb171e2f8dd15929e598d2cbb0916f43cf57a5
rnwnsgud <rnwnsgud90@naver.com>
2023-09-22 19:20:54 +0900

    connecting

---
+++ src/main/java/co/kr/ppingpong/PpingpongApplication.java
@@ -0,0 +1,13 @@
+package co.kr.ppingpong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpingpongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpingpongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/kr/ppingpong/PpingpongApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.ppingpong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpingpongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
