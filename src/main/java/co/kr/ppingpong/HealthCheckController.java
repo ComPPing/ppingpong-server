f013fd7b561aed4b61bcb627eab47f1ab47ef6eb
rnwnsgud <rnwnsgud90@naver.com>
2023-09-23 16:12:25 +0900

    health checking 주소 생성

---
+++ src/main/java/co/kr/ppingpong/HealthCheckController.java
@@ -0,0 +1,14 @@
+package co.kr.ppingpong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/")
+    public String hi() {
+        return "hi";
+    }
+
+}
