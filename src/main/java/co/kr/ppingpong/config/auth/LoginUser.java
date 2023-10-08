630b7bedcfc315c31aab67d19f14c2d7f7cd2f99
구준형 <78197563+rnwnsgud@users.noreply.github.com>
2023-10-09 03:15:33 +0900

    Update LoginUser.java

--- src/main/java/co/kr/ppingpong/config/auth/LoginUser.java
+++ src/main/java/co/kr/ppingpong/config/auth/LoginUser.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+
 import java.util.Collection;
 import java.util.Map;
 
@@ -12,12 +13,26 @@ import java.util.Map;
 @Getter
 public class LoginUser implements UserDetails, OAuth2User { //
 
-
     private final User user;
     private Map<String, Object> attributes;
 
     public LoginUser(User user) {
         this.user = user;
+    }
+
+    public LoginUser(User user, Map<String, Object> attributes) {
+        this.user = user;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public String getName() {
+        return (String) attributes.get("name");
     }
 
     @Override
